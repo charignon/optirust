c7fcabedf2c21824b16d1dd29841a2ea5823cc13
Laurent Charignon <l.charignon@gmail.com>
2018-02-12 00:28:28 -0800

    Reorganize code in types.rs

--- src/types.rs
+++ src/types.rs
@@ -13,6 +13,7 @@ use gen;
 use serde_yaml;
 use solver;
 use chrono_tz::Tz;
+use std::fmt;
 use yaml_rust;
 use fixtures::{test_config, test_input, test_invalid_input};
 pub type ScoringFnType = Box<
@@ -115,6 +116,124 @@ fn compute_score(
     score
 }
 
+// TODO Remove all this and use serde_yaml properly
+// We use YAML for all the format, each of the pub struct can implement the
+// YamlParsable trait for easy deserialization
+pub trait YamlParsable {
+    // Given a Yaml mapping to a pub struct, parses and return an instance of it
+    fn from_yaml(s: &yaml_rust::Yaml) -> Self;
+}
+
+fn parse_list_of<T: YamlParsable>(s: &yaml_rust::Yaml) -> Vec<T> {
+    s.as_vec()
+        .unwrap()
+        .into_iter()
+        .map(|x| T::from_yaml(x))
+        .collect()
+}
+// The config file format describes the small and large
+// rooms available to book, small means 2 people or less
+// large 3+ people
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Config {
+    pub small_rooms: Vec<String>,
+    pub large_rooms: Vec<String>,
+}
+
+impl Config {
+    pub fn from_yaml_str(s: &str) -> Config {
+        serde_yaml::from_str(&s).unwrap()
+    }
+
+    pub fn room_picker(&self, size: usize) -> Vec<String> {
+        if size <= 2 {
+            self.small_rooms.clone()
+        } else {
+            self.large_rooms.clone()
+        }
+    }
+
+    pub fn from_file(file: &str) -> Config {
+        let mut config = File::open(file).expect("file not found");
+        let mut contents = String::new();
+        config
+            .read_to_string(&mut contents)
+            .expect("something went wrong reading the file");
+        Config::from_yaml_str(&contents)
+    }
+}
+// A potential meeting, linked to a desired meeting
+#[derive(Clone, PartialEq)]
+pub struct MeetingCandidate {
+    pub title: String,
+    pub id: String,
+    pub start: DateTime<chrono::Utc>,
+    pub end: DateTime<chrono::Utc>,
+    pub room: String,
+    pub score: usize,
+}
+
+impl MeetingCandidate {
+    #[allow(dead_code)]
+    pub fn intersects(&self, o: &MeetingCandidate) -> bool {
+        (self.start < o.end) && (o.start < self.end)
+    }
+}
+
+impl fmt::Debug for MeetingCandidate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Candidate {:?} {{ start_time: {:?}, end_time: {:?}, room: {:?}, score: {:?} }}",
+            self.id,
+            self.start.with_timezone(&chrono::Local).to_string(),
+            self.end.with_timezone(&chrono::Local).to_string(),
+            self.room,
+            self.score
+        )
+    }
+}
+impl fmt::Debug for Solution {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let solved = format!("Solved: {:?}\n", self.solved);
+        let candidates_str = self.candidates
+            .iter()
+            .map(|it| format!("{:?}\n >>> {:?}\n", it.0, it.1))
+            .collect::<Vec<String>>()
+            .join("===============================\n");
+        write!(f, "{}\n{}\n", solved, candidates_str)
+    }
+}
+
+// A meeting that is already scheduled before the program runs
+// There meetings are stored as a tree for easy intersection
+// computation
+#[derive(Debug)]
+pub struct Meeting {
+    pub id: String,
+    pub start: DateTime<chrono::Utc>,
+    pub end: DateTime<chrono::Utc>,
+}
+
+impl YamlParsable for Meeting {
+    fn from_yaml(s: &yaml_rust::Yaml) -> Meeting {
+        let start_time = s["start"]["dateTime"].as_str().unwrap();
+        let end_time = s["end"]["dateTime"].as_str().unwrap();
+
+        Meeting {
+            id: s["id"].as_str().unwrap().to_string(),
+            start: start_time.parse::<DateTime<Utc>>().unwrap(),
+            end: end_time.parse::<DateTime<Utc>>().unwrap(),
+        }
+    }
+}
+pub type MeetingsTree = IntervalTree<DateTime<chrono::Utc>, String>;
+
+pub struct Solution {
+    pub solved: bool,
+    pub candidates: HashMap<DesiredMeeting, MeetingCandidate>,
+}
+
 // The input to the program containing what meetings the user
 // wants to schedule
 pub struct Input {
@@ -151,23 +270,12 @@ impl Input {
     }
 }
 
-#[test]
-#[should_panic]
-fn panic_two_desired_meeting_same_title() {
-    Input::from_yaml_str(&test_invalid_input());
-}
-
-#[test]
-fn can_build_input() {
-    let a = Input::from_yaml_str(&test_input());
-    assert_eq!(a.meetings[0].title, "title");
-    assert_eq!(a.meetings[0].description, "description");
-    assert_eq!(a.meetings[0].attendees[0], "laurent.charignon@foo.com");
-    assert_eq!(a.meetings[0].timezone.name(), "America/Los_Angeles");
-    // 10 PST -> 18 UTC
-    // 18 PST -> 2 UTC
-    assert_eq!(a.meetings[0].min_date.hour(), 18);
-    assert_eq!(a.meetings[0].max_date.hour(), 2);
+impl YamlParsable for Input {
+    fn from_yaml(s: &yaml_rust::Yaml) -> Input {
+        Input {
+            meetings: parse_list_of(&s["meetings"]),
+        }
+    }
 }
 
 #[derive(Eq, PartialEq, Clone, Debug)]
@@ -187,118 +295,6 @@ impl Hash for DesiredMeeting {
         self.title.hash(state);
         self.description.hash(state);
     }
-}
-
-// The config file format describes the small and large
-// rooms available to book, small means 2 people or less
-// large 3+ people
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct Config {
-    pub small_rooms: Vec<String>,
-    pub large_rooms: Vec<String>,
-}
-
-impl Config {
-    pub fn from_yaml_str(s: &str) -> Config {
-        serde_yaml::from_str(&s).unwrap()
-    }
-
-    pub fn room_picker(&self, size: usize) -> Vec<String> {
-        if size <= 2 {
-            self.small_rooms.clone()
-        } else {
-            self.large_rooms.clone()
-        }
-    }
-
-    pub fn from_file(file: &str) -> Config {
-        let mut config = File::open(file).expect("file not found");
-        let mut contents = String::new();
-        config
-            .read_to_string(&mut contents)
-            .expect("something went wrong reading the file");
-        Config::from_yaml_str(&contents)
-    }
-}
-#[test]
-fn can_build_config() {
-    let a = Config::from_yaml_str(&test_config());
-    assert_eq!(a.large_rooms[0], "bozorg@jam.com")
-}
-
-// A potential meeting, linked to a desired meeting
-#[derive(Clone, PartialEq)]
-pub struct MeetingCandidate {
-    pub title: String,
-    pub id: String,
-    pub start: DateTime<chrono::Utc>,
-    pub end: DateTime<chrono::Utc>,
-    pub room: String,
-    pub score: usize,
-}
-
-impl MeetingCandidate {
-    #[allow(dead_code)]
-    pub fn intersects(&self, o: &MeetingCandidate) -> bool {
-        (self.start < o.end) && (o.start < self.end)
-    }
-}
-
-use std::fmt;
-impl fmt::Debug for MeetingCandidate {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Candidate {:?} {{ start_time: {:?}, end_time: {:?}, room: {:?}, score: {:?} }}",
-            self.id,
-            self.start.with_timezone(&chrono::Local).to_string(),
-            self.end.with_timezone(&chrono::Local).to_string(),
-            self.room,
-            self.score
-        )
-    }
-}
-impl fmt::Debug for Solution {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let solved = format!("Solved: {:?}\n", self.solved);
-        let candidates_str = self.candidates
-            .iter()
-            .map(|it| format!("{:?}\n >>> {:?}\n", it.0, it.1))
-            .collect::<Vec<String>>()
-            .join("===============================\n");
-        write!(f, "{}\n{}\n", solved, candidates_str)
-    }
-}
-
-// A meeting that is already scheduled before the program runs
-// There meetings are stored as a tree for easy intersection
-// computation
-#[derive(Debug)]
-pub struct Meeting {
-    pub id: String,
-    pub start: DateTime<chrono::Utc>,
-    pub end: DateTime<chrono::Utc>,
-}
-pub type MeetingsTree = IntervalTree<DateTime<chrono::Utc>, String>;
-
-pub struct Solution {
-    pub solved: bool,
-    pub candidates: HashMap<DesiredMeeting, MeetingCandidate>,
-}
-
-// We use YAML for all the format, each of the pub struct can implement the
-// YamlParsable trait for easy deserialization
-pub trait YamlParsable {
-    // Given a Yaml mapping to a pub struct, parses and return an instance of it
-    fn from_yaml(s: &yaml_rust::Yaml) -> Self;
-}
-
-fn parse_list_of<T: YamlParsable>(s: &yaml_rust::Yaml) -> Vec<T> {
-    s.as_vec()
-        .unwrap()
-        .into_iter()
-        .map(|x| T::from_yaml(x))
-        .collect()
 }
 
 impl YamlParsable for DesiredMeeting {
@@ -335,23 +331,27 @@ impl YamlParsable for DesiredMeeting {
     }
 }
 
-impl YamlParsable for Input {
-    fn from_yaml(s: &yaml_rust::Yaml) -> Input {
-        Input {
-            meetings: parse_list_of(&s["meetings"]),
-        }
-    }
+#[test]
+#[should_panic]
+fn panic_two_desired_meeting_same_title() {
+    Input::from_yaml_str(&test_invalid_input());
 }
 
-impl YamlParsable for Meeting {
-    fn from_yaml(s: &yaml_rust::Yaml) -> Meeting {
-        let start_time = s["start"]["dateTime"].as_str().unwrap();
-        let end_time = s["end"]["dateTime"].as_str().unwrap();
+#[test]
+fn can_build_input() {
+    let a = Input::from_yaml_str(&test_input());
+    assert_eq!(a.meetings[0].title, "title");
+    assert_eq!(a.meetings[0].description, "description");
+    assert_eq!(a.meetings[0].attendees[0], "laurent.charignon@foo.com");
+    assert_eq!(a.meetings[0].timezone.name(), "America/Los_Angeles");
+    // 10 PST -> 18 UTC
+    // 18 PST -> 2 UTC
+    assert_eq!(a.meetings[0].min_date.hour(), 18);
+    assert_eq!(a.meetings[0].max_date.hour(), 2);
+}
 
-        Meeting {
-            id: s["id"].as_str().unwrap().to_string(),
-            start: start_time.parse::<DateTime<Utc>>().unwrap(),
-            end: end_time.parse::<DateTime<Utc>>().unwrap(),
-        }
-    }
+#[test]
+fn can_build_config() {
+    let a = Config::from_yaml_str(&test_config());
+    assert_eq!(a.large_rooms[0], "bozorg@jam.com")
 }
